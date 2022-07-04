f2f5dc478a38831de757b1fe0478344e8d0d49b8
Caio Fernandes <caiovf13@gmail.com>
2022-07-04 14:46:16 -0300

    📦 NEW: Add ssm endpoint value accordingly with cluster mode

--- ssm.tf
+++ ssm.tf
@@ -2,9 +2,8 @@ resource "aws_ssm_parameter" "redis_endpoint" {
   name        = "/elasticache/redis/${var.env}-${var.name}/ENDPOINT"
   description = "Elasticache Redis Endpoint"
   type        = "String"
-  value       = aws_elasticache_replication_group.redis.primary_endpoint_address
+  value       = aws_elasticache_replication_group.redis.cluster_enabled ? aws_elasticache_replication_group.redis.configuration_endpoint_address : aws_elasticache_replication_group.redis.primary_endpoint_address
 }
-
 resource "aws_ssm_parameter" "redis_port" {
   name        = "/elasticache/redis/${var.env}-${var.name}/PORT"
   description = "Elasticache Redis Port"
