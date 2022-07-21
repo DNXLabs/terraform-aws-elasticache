99a3cb0a51acd475d08b64a1a25e8f2202871ff8
Allan Denot <adenot@gmail.com>
2022-07-21 01:15:55 +0000

    Allow selecting secret method for output

---
+++ secrets-manager.tf
@@ -0,0 +1,15 @@
+resource "aws_secretsmanager_secret" "elasticache" {
+  count                   = var.secret_method == "secretsmanager" ? 1 : 0
+  name                    = "/elasticache/${var.env}-${var.name}"
+  recovery_window_in_days = 0
+}
+
+resource "aws_secretsmanager_secret_version" "elasticache" {
+  count         = var.secret_method == "secretsmanager" ? 1 : 0
+  secret_id     = aws_secretsmanager_secret.elasticache[0].id
+  secret_string = jsonencode({
+    "REDIS_PORT": aws_elasticache_replication_group.redis.port,
+    "REDIS_HOST": aws_elasticache_replication_group.redis.cluster_enabled ? aws_elasticache_replication_group.redis.configuration_endpoint_address : aws_elasticache_replication_group.redis.primary_endpoint_address,
+    "REDIS_URL": "redis://${aws_elasticache_replication_group.redis.cluster_enabled ? aws_elasticache_replication_group.redis.configuration_endpoint_address : aws_elasticache_replication_group.redis.primary_endpoint_address}",
+  })
+}
