8c5a36dade44a82a16e9321dc36a4aef579b0ed9
Allan Denot <adenot@gmail.com>
2022-07-21 06:21:25 +0000

    terraform fmt

--- secrets-manager.tf
+++ secrets-manager.tf
@@ -5,11 +5,11 @@ resource "aws_secretsmanager_secret" "elasticache" {
 }
 
 resource "aws_secretsmanager_secret_version" "elasticache" {
-  count         = var.secret_method == "secretsmanager" ? 1 : 0
-  secret_id     = aws_secretsmanager_secret.elasticache[0].id
+  count     = var.secret_method == "secretsmanager" ? 1 : 0
+  secret_id = aws_secretsmanager_secret.elasticache[0].id
   secret_string = jsonencode({
-    "REDIS_PORT": aws_elasticache_replication_group.redis.port,
-    "REDIS_HOST": aws_elasticache_replication_group.redis.cluster_enabled ? aws_elasticache_replication_group.redis.configuration_endpoint_address : aws_elasticache_replication_group.redis.primary_endpoint_address,
-    "REDIS_URL": "redis://${aws_elasticache_replication_group.redis.cluster_enabled ? aws_elasticache_replication_group.redis.configuration_endpoint_address : aws_elasticache_replication_group.redis.primary_endpoint_address}",
+    "REDIS_PORT" : aws_elasticache_replication_group.redis.port,
+    "REDIS_HOST" : aws_elasticache_replication_group.redis.cluster_enabled ? aws_elasticache_replication_group.redis.configuration_endpoint_address : aws_elasticache_replication_group.redis.primary_endpoint_address,
+    "REDIS_URL" : "redis://${aws_elasticache_replication_group.redis.cluster_enabled ? aws_elasticache_replication_group.redis.configuration_endpoint_address : aws_elasticache_replication_group.redis.primary_endpoint_address}",
   })
 }
