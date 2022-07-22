704999e880b350902867613e83d9983570bd96dc
Allan Denot <adenot@gmail.com>
2022-07-22 20:22:56 +1000

    Adding rediss:// url when encryption in transit (#9)

--- secrets-manager.tf
+++ secrets-manager.tf
@@ -10,6 +10,6 @@ resource "aws_secretsmanager_secret_version" "elasticache" {
   secret_string = jsonencode({
     "REDIS_PORT" : aws_elasticache_replication_group.redis.port,
     "REDIS_HOST" : aws_elasticache_replication_group.redis.cluster_enabled ? aws_elasticache_replication_group.redis.configuration_endpoint_address : aws_elasticache_replication_group.redis.primary_endpoint_address,
-    "REDIS_URL" : "redis://${aws_elasticache_replication_group.redis.cluster_enabled ? aws_elasticache_replication_group.redis.configuration_endpoint_address : aws_elasticache_replication_group.redis.primary_endpoint_address}:${aws_elasticache_replication_group.redis.port}",
+    "REDIS_URL" : "redis${var.transit_encryption_enabled ? "s" : ""}://${aws_elasticache_replication_group.redis.cluster_enabled ? aws_elasticache_replication_group.redis.configuration_endpoint_address : aws_elasticache_replication_group.redis.primary_endpoint_address}:${aws_elasticache_replication_group.redis.port}",
   })
 }
