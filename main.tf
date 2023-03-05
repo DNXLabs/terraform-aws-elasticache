8b19f7ff629fc85c20290b18170e94529153dd64
matthosking-mydeal <95453715+matthosking-mydeal@users.noreply.github.com>
2023-03-05 14:48:19 +1100

    use aws_subnets instead of previous aws_subnet_ids

--- main.tf
+++ main.tf
@@ -65,5 +65,5 @@ resource "aws_elasticache_parameter_group" "redis_parameter_group" {
 
 resource "aws_elasticache_subnet_group" "redis_subnet_group" {
   name       = replace(format("%.255s", lower(replace("tf-redis-${var.name}-${var.env}-${var.vpc_id}", "_", "-"))), "/\\s/", "-")
-  subnet_ids = data.aws_subnet_ids.selected.ids
+  subnet_ids = data.aws_subnets.selected.ids
 }
