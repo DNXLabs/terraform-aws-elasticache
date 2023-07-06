cd17a8328cc610b1c1e38414a40afd699d046b7e
lucastercas <lucas.macedo@dnx.solutions>
2023-07-05 21:04:28 -0300

    Add dynamic filter for subnet filter tags

--- _data.tf
+++ _data.tf
@@ -3,13 +3,12 @@ data "aws_vpc" "selected" {
 }
 
 data "aws_subnets" "selected" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.selected.id]
-  }
-
-  filter {
-    name   = "tag:Name"
-    values = ["Private*"]
+  dynamic "filter" {
+    for_each = var.subnet_tags_filter
+    iterator = tag
+    content {
+      name   = tag.key
+      values = [tag.value]
+    }
   }
 }
